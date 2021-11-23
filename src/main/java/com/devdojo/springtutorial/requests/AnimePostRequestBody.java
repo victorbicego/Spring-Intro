5473671145541ec662c7b0916ce6c052aeeeaa46
Victor Bicego Quintaneira <victor@Victors-MacBook-Air.local>
2021-11-23 16:52:47 +0100

    Validation

--- src/main/java/com/devdojo/springtutorial/requests/AnimePostRequestBody.java
+++ src/main/java/com/devdojo/springtutorial/requests/AnimePostRequestBody.java
@@ -1,8 +1,14 @@
 package com.devdojo.springtutorial.requests;
 
 import lombok.Data;
+import org.springframework.lang.Nullable;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class AnimePostRequestBody {
+    @NotNull(message = "The anime name cannot be null")
+    @NotEmpty(message = "The anime name cannot be empty")
     private String name;
 }
